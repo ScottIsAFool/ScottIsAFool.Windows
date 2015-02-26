6244cfcbdeaa6ab45435bc885fa9164207c7ec70
Scott Lovegrove <scottisafool@live.co.uk>
2015-02-26 18:17:10 +0000

    Added extra list extension methods

--- ScottIsAFool.Windows.Core (WPA81)/Extensions/ListExtensions.cs
+++ ScottIsAFool.Windows.Core (WPA81)/Extensions/ListExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ScottIsAFool.Windows.Core.Extensions
@@ -10,9 +11,19 @@ namespace ScottIsAFool.Windows.Core.Extensions
             return list == null || !list.Any();
         }
 
+        public static bool IsNullOrEmpty<T>(this Stack<T> stack)
+        {
+            return stack == null || !stack.Any();
+        }
+
+        public static List<T> ToList<T>(this Array array)
+        {
+            return array.Cast<T>().ToList();
+        } 
+
         public static string ToQueryString(this Dictionary<string, string> postData)
         {
-            var items = postData.Select(x => string.Format("{0}={1}", x.Key, x.Value));
+            var items = postData.Select(x => string.Format	("{0}={1}", x.Key, x.Value));
 
             return string.Join("&", items);
         }
