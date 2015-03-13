63e64ff0041bf98e6bbaf98450a68bbd93c78ac1
ScottIsAFool <scottisafool@live.co.uk>
2015-03-13 23:13:43 +0000

    Added MvvmLight projects

---
+++ ScottIsAFool.Windows.MvvmLight (WPA81)/ViewModel/ViewModelBase.cs
@@ -0,0 +1,48 @@
+﻿using ScottIsAFool.Windows.Core.Logging;
+
+namespace ScottIsAFool.Windows.MvvmLight.ViewModel
+{
+    public abstract class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase
+    {
+        public ILog Log { get; set; }
+
+        protected ViewModelBase()
+        {
+            if (!IsInDesignMode)
+            {
+                WireMessages();
+                Log = new WinLogger(GetType().FullName);
+            }
+        }
+
+        protected virtual void WireMessages()
+        {
+        }
+
+        public virtual bool IsPinned
+        {
+            get { return false; }
+        }
+
+        public void SetProgressBar(string text)
+        {
+            ProgressIsVisible = true;
+            ProgressText = text;
+
+            UpdateProperties();
+        }
+
+        public void SetProgressBar()
+        {
+            ProgressIsVisible = false;
+            ProgressText = string.Empty;
+
+            UpdateProperties();
+        }
+
+        public bool ProgressIsVisible { get; set; }
+        public string ProgressText { get; set; }
+
+        public virtual void UpdateProperties() { }
+    }
+}
